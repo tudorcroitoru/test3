3405e972d62c72ae287b35dcd60b7a49eda6e4f8
tudorcroitoru <croitoru.tudor2000@gmail.com>
2021-02-27 11:37:19 +0200

    update 1.3

--- src/Shot.java
+++ src/Shot.java
@@ -2,5 +2,6 @@ public class Shot {
     public static void main(String[] args) {
         System.out.println("Shot");
         System.out.println("Second update");
+        System.out.println("update 1.3");
     }
 }

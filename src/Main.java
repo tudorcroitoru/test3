fe56d52790f55d7f2eb76483c184c76a295bc7e7
tudorcroitoru <croitoru.tudor2000@gmail.com>
2021-02-27 11:30:14 +0200

    update 1.2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First Text");
         System.out.println("Second Text");
+        System.out.println("Third Text");
     }
 }
 
